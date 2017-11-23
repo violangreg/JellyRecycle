032ca49815d1ff921e628747cea0dab68a621182
Luis Pedroza-Soto <luis.g.pedrozasoto@gmail.com>
2017-11-23 15:33:24 -0800

    Version 1.0
    Initial deployable version of Project Green

---
+++ Assets/_Scripts/PlasticBottleGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlasticBottleGenerator : MonoBehaviour
+{
+
+    public Transform generationPoint;
+    public GameObject cola;
+    //public ObjectPooler objectPool;
+
+    // Use this for initialization
+    void Start()
+    {
+        InvokeRepeating("spawnBottle", 1.0f, 1.0f);
+    }
+
+    // Update is called once per frame
+    void update()
+    {
+        spawnBottle();
+    }
+
+    void spawnBottle()
+    {
+        Instantiate(cola, new Vector3(Random.Range(-5, 5), 0), cola.transform.rotation);
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# Project Green
+
+## BeachHacks Hackathon Project
+
+### Written by Steven Kang, Luis Guillermo Pedroza-Soto, and Greg Violan
+### Graphic design by Sarah Han
+
+Our entry for the BeachHacks Hackathon series. We chose to not only make a game, but to also
+raise some awareness about recycling in the process. This project was pretty fun to do and I hope
+users enjoy. All lefts reserved.
